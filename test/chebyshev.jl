85e52bfc687f4dec3522048c1e35e618db2acd22
Jeff Eldredge <jdeldre@g.ucla.edu>
2024-06-02 14:38:30 -0700

    Added a few tests for solution of Chebyshev problems

--- test/chebyshev.jl
+++ test/chebyshev.jl
@@ -39,3 +39,31 @@ end
 
 
 end
+
+@testset "Solution of equations" begin
+
+    Nx = 128
+    x = cos.(π*(0:Nx)/Nx)
+    uex = (1 .- x.^2)/2
+
+    D1 = PseudoSpectral._construct_chebd1(Nx)
+    D2 = PseudoSpectral._construct_chebd2(Nx,D1)
+
+    A = copy(D2)
+    A[1,:] .= 0
+    A[1,1] = 1
+    A[Nx+1,:] .= 0
+    A[Nx+1,Nx+1] = 1
+
+    b = zeros(Nx+1)
+    b[2:Nx] .= -1
+
+    u = A\b
+    @test norm(u-uex) < 1e-9
+
+    A[1,:] .= D1[1,:]
+    uex = 2*(1 .- 0.25*(x .- 1).^2)
+    u = A\b
+
+    @test norm(u-uex) < 1e-9
+end
